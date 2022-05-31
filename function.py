ceddac57719e1e6b85f34e4d7bbf0cbc392e9330
hongzzi-zzi <ahnhe9227@naver.com>
2022-05-31 07:17:29 +0000

    function

--- function.py
+++ function.py
@@ -32,7 +32,7 @@ def remove_distractors_duplicate_with_correct_answer(correct: str, distractors: 
 Code from: https://github.com/allenai/bi-att-flow/blob/master/squad/evaluate-v1.1.py
 '''
 def _normalize_item(item) -> str:
-    """Lower text and remove punctuation, articles and extra whitespace."""
+    # Lower text and remove punctuation, articles and extra whitespace.
     def remove_articles(text):
         return re.sub(r'\b(a|an|the)\b', ' ', text)
 
@@ -73,14 +73,6 @@ def _calculate_nltk_bleu(references: List[str], hypothesis: str, bleu_n: int = 1
 #%% text_cleaning.py in leaf
 
 def clean_text(text: str) -> str:
-    """Clean the text from symbols and additional information.
-    
-    Args:
-        text (str): The text.
-    
-    Returns:
-        str: CLeaned text.
-    """
     cleaned_text = _remove_brackets(text)
     cleaned_text = _remove_square_brackets(cleaned_text)
     cleaned_text = _remove_multiple_spaces(cleaned_text)
@@ -90,59 +82,20 @@ def clean_text(text: str) -> str:
     
 
 def _remove_brackets(text: str) -> str:
-    """ Remove brackets '(', ')' and the information between them. 
-
-    e.g. "The koala has a body length of 60–85 cm (24–33 in)."
-    
-    Args:
-        text (str): The text.
-    
-    Returns:
-        str: CLeaned text.
-    """
+    # Remove brackets '(', ')' and the information between them.
     return re.sub(r'\((.*?)\)', lambda L: '', text)
 
 
 def _remove_square_brackets(text: str) -> str:
-    """ Remove square brackets '[', ']' and the information between them. 
-
-    e.g. The koala[1] is cool."
-    
-    Args:
-        text (str): The text.
-    
-    Returns:
-        str: CLeaned text.
-    """
-
+    # Remove square brackets '[', ']' and the information between them.
     return re.sub(r'\[(.*?)\]', lambda L: '', text)
 
 
 def _remove_multiple_spaces(text: str) -> str:
-    """Remove multiple white spaces. 
-
-    e.g. "The koala         is     angry  !"
-    
-    Args:
-        text (str): The text.
-    
-    Returns:
-        str: CLeaned text.
-    """
-
+    # Remove multiple white spaces. 
     return re.sub(' +', ' ', text)
 
 
 def _replace_weird_hyphen(text: str) -> str:
-    """ Replace weird '–' hyphen that's not recognized as a delimeter by spacy. 
-
-    e.g. '4–15 kg' -> '4-15 kg' 
-    (You may not see a difference, but there fucking is. This motherfucker '–' is not recognized by spacy as a delimeter.)
-    
-    Args:
-        text (str): The text.
-    
-    Returns:
-        str: CLeaned text.
-    """
+    #  Replace weird '–' hyphen that's not recognized as a delimeter by spacy.
     return text.replace('–', '-')
